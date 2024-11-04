18381b751a4271382e72008bca1ba7af126458d6
arafat-sabbir <arafatshabbir8@gmail.com>
2024-11-05 01:45:29 +0600

    Complete Query 5 And 6

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -58,7 +58,6 @@ VALUES
 insert into students VALUES(7,'Arafat Sabbir',21,'arafat8@gmail.com',60,60,NULL)
 
 -- Query (2).Retrieve the names of all students who are enrolled in the course titled 'Next.js'.:
-
 select * from enrollments
 join students on students.student_id = enrollments.student_id
 where enrollments.course_id = (select course_id from courses where course_name = 'Next.js')
@@ -69,6 +68,25 @@ update students
 set status = 'Awarded'
 where (frontend_mark+backend_mark) = ( select max(frontend_mark+backend_mark) from students); 
 
+-- Query (4).Delete all courses that have no students enrolled.:
+delete from students 
+where student_id not in (select student_id from enrollments);
 
--- Query (4).Retrieve the names of students using a limit of 2, starting from the 3rd student..:
+
+-- Query (5).Retrieve the names of students using a limit of 2, starting from the 3rd student.:
 select student_name from students limit 2 offset 2;
+
+
+
+-- Query (6).Retrieve the course names and the number of students enrolled in each course.:
+SELECT 
+    courses.course_name,
+    COUNT(enrollments.student_id) AS student_count
+FROM 
+    courses
+JOIN 
+    enrollments ON courses.course_id = enrollments.course_id
+GROUP BY 
+    courses.course_name;
+
+
